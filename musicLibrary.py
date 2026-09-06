783a498ecf1aa837b155f024d6e312cd7e2ff093
Ayush Sharma <ayush.sharma1873@gmail.com>
2026-09-06 19:50:12 +0530

    Remove MusicLibrary file

--- musicLibrary.py
+++
@@ -1,7 +0,0 @@
-music = {
-    "senorita": "https://www.youtube.com/watch?v=2Z0Put0teCM&pp=ygUIc2Vub3JpdGE%3D",
-    "beggin": "https://www.youtube.com/watch?v=W2MpGCL8-9o&pp=ygUGYmVnZ2lu",
-    "shape of you":"https://www.youtube.com/watch?v=JGwWNGJdvx8&pp=ygUMc2hhcGUgb2YgeW91",
-    "fairytale":"https://www.youtube.com/watch?v=WXwgZL4zx9o&pp=ygUJZmFpcnl0YWxl",
-    "despacito":"https://www.youtube.com/watch?v=kJQP7kiw5Fk&pp=ygUJZGVzcGFjaXRv",
-}
